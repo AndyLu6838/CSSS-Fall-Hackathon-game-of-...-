4d4eaad50aa3765216268e8214d2fcec3d2c5575
Ryan Lintott <ryan.lintott@generac.com>
2020-11-07 16:53:46 -0800

    enabling rule configuration

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -62,24 +62,25 @@ int main(){
     string overpopulationLimitStr;
     string neighbourRadiusStr;
     string filename;
-	
+
     cout << "                        CSSS FALL HACKATHON - THE GAME OF ... - Implementation in C++" << endl;
 	 // TODO:  write introduction and get rid of this one
     cout << endl;
     cout << endl;
     cout << endl;
-    cout << "Based on cellular automaton created by the British mathematician John Horton Conway in 1970." << endl;
+    cout << "Also known simply as life, " << endl;
+    cout << "is a cellular automaton devised by the British mathematician John Horton Conway in 1970." << endl;
     cout << endl;
-    cout << "We have the Game of ________" << endl;
-    cout << "The game consists of an infinite two-dimensional orthogonal grid of square cells, where each cell is either alive or dead." << endl;
-    cout << endl;
-    cout << "Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent." << endl;
-    cout << "After each generation, the following transitions occur:" << endl;
+    cout << "Rules" << endl;
+    cout << "The universe of the Game of life is an infinite two-dimensional orthogonal grid of square cells," << endl;
+    cout << "each of which is in one of two possible states, life or dead. Every" << endl;
+    cout << "cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent." << endl;
+    cout << "At each step in time, the following transitions occur:" << endl;
     cout << "1. Any live cell with fewer than two live neighbours dies, as if caused by under-population." << endl;
     cout << "2. Any live cell with two or three live neighbours lives on to the next generation." << endl;
     cout << "3. Any live cell with more than three live neighbours dies, as if by over-population." << endl;
     cout << "4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction." << endl;
-    cout << endl << "We will denote the cells as following: "<< endl;
+    cout << endl;
     cout << "O - living cell" << endl;
     cout << ". - dead cell" << endl;
     cout << endl;
@@ -147,16 +148,16 @@ int main(){
       }
     else
       {
-	
-	for(int i=0;i<stoi(nc);i++)
-	  {
-	    cout << "Enter the coordinates of cell " << i+1 << " : ";
-	    cin >> x >> y;
-	    gridOne[x][y] = true;
-	    printGrid(gridOne);
-	  }
+        for(int i=0;i<stoi(nc);i++)
+          {
+            cout <<stoi(nc)<< "Enter the coordinates of cell " << i+1 << " : ";
+            cin >> x >> y;
+            gridOne[x][y] = true;
+            printGrid(gridOne);
+          }
       }
-	
+  
+  
     cout << "Grid setup is done. Play with the default Game of Life rules? (y/n)" << endl;
 
     cin >> isCustomRules;
@@ -179,9 +180,10 @@ int main(){
       cin >> neighbourRadiusStr;
       neighbourRadius = stoi(neighbourRadiusStr);
     }
-		
-    cout << "Rule configuration is done. Start the game ? (y/n)" << endl;
 	
+	
+    cout << "Rule configuration is done. Start the game ? (y/n)" << endl;
+
     printGrid(gridOne);
     cin >> start;
     if( start == "y" || start == "Y" )
